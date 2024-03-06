0bffd187517bbcbb41380d3a54f9809f772fd98e
nianshangy <915113788@qq.com>
2024-03-06 22:23:03 +0800

    feature：通过分布式锁防止毫秒级的大量恶意请求去注册一个未注册的用户名

---
+++ admin/src/main/java/com/nian/shortlink/admin/common/constat/RedisCacheConstant.java
@@ -0,0 +1,8 @@
+package com.nian.shortlink.admin.common.constat;
+
+/**
+ * 短链接后管项目 redis 的缓存常量类
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user_register:";
+}
--- admin/src/main/java/com/nian/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/nian/shortlink/admin/service/impl/UserServiceImpl.java
@@ -11,29 +11,34 @@ import com.nian.shortlink.admin.mapper.UserMapper;
 import com.nian.shortlink.admin.service.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import static com.nian.shortlink.admin.common.constat.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
+
 /**
-* @author nianshang
-* @description 针对表【t_user】的数据库操作Service实现
-* @createDate 2024-03-03 20:05:57
-*/
+ * @author nianshang
+ * @description 针对表【t_user】的数据库操作Service实现
+ * @createDate 2024-03-03 20:05:57
+ */
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
-    implements IUserService {
+        implements IUserService {
 
     private final RBloomFilter<String> userRegisterCacheBloomFilter;
+    private final RedissonClient redissonClient;
 
     @Override
     public UserVO queryByUsername(String username) {
         User user = lambdaQuery().eq(User::getUsername, username).one();
-        if(user == null){
-            throw new ClientException("输入的用户不存在！",UserErrorCode.USER_NULL);
+        if (user == null) {
+            throw new ClientException("输入的用户不存在！", UserErrorCode.USER_NULL);
         }
         UserVO result = new UserVO();
-        BeanUtils.copyProperties(user,result);
+        BeanUtils.copyProperties(user, result);
         return result;
     }
 
@@ -52,17 +57,27 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     @Override
     public void userRegister(UserDTO requestParam) {
         //1.判断用户是否存在
-        if(!hasUsername(requestParam.getUsername())){
+        if (!hasUsername(requestParam.getUsername())) {
             throw new ClientException(UserErrorCode.USER_NAME_EXIST);
         }
-        //2.新增到数据库
-        User user = new User();
-        BeanUtil.copyProperties(requestParam,user);
-        boolean save = save(user);
-        if(!save){
-            throw new ClientException(UserErrorCode.USER_SAVE_ERROR);
+        //2.根据用户名去获取分布式锁(注意：后面还需要加上用户名，不然就是所以用户获取同一把锁了)
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + requestParam.getUsername());
+        try {
+            if (lock.tryLock()) {
+                //2.新增到数据库
+                User user = new User();
+                BeanUtil.copyProperties(requestParam, user);
+                boolean save = save(user);
+                if (!save) {
+                    throw new ClientException(UserErrorCode.USER_SAVE_ERROR);
+                }
+                //3.每次注册用户时需要去将用户名存储进布隆过滤器中
+                userRegisterCacheBloomFilter.add(requestParam.getUsername());
+                return;
+            }
+            throw new ClientException(UserErrorCode.USER_NAME_EXIST);
+        } finally {
+            lock.unlock();
         }
-        //3.每次注册用户时需要去将用户名存储进布隆过滤器中
-        userRegisterCacheBloomFilter.add(requestParam.getUsername());
     }
 }
