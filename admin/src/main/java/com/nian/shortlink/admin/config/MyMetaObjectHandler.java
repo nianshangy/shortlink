59967b75850a7310a61ba2a386145b97680b0698
nianshangy <915113788@qq.com>
2024-03-06 21:57:59 +0800

    feature：开发短链接后管项目通用持久层对象属性自动填充

---
+++ admin/src/main/java/com/nian/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,24 @@
+package com.nian.shortlink.admin.config;
+
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        //数据库中的创建时间与更新时间都是date类型的，所以后面需要设置成Date的字节码文件
+        strictInsertFill(metaObject, "create_time", Date::new, Date.class);
+        strictInsertFill(metaObject, "update_time", Date::new, Date.class);
+        strictInsertFill(metaObject, "del_flag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        strictInsertFill(metaObject, "update_time", Date::new, Date.class);
+    }
+}
