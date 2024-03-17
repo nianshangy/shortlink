16ab4e22d96c268b755292bab7b80f763b126e37
nianshangy <915113788@qq.com>
2024-03-17 18:49:24 +0800

    feature：修改短链接后管用户上下文，增加路径忽略

--- admin/src/main/java/com/nian/shortlink/admin/config/UserConfiguration.java
+++ admin/src/main/java/com/nian/shortlink/admin/config/UserConfiguration.java
@@ -1,7 +1,6 @@
 package com.nian.shortlink.admin.config;
 
 import com.nian.shortlink.admin.common.biz.user.UserTransmitFilter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
