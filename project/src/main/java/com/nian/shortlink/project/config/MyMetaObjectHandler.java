875d81efbc8a08377d78866606a22c1ce16aeb43
nianshangy <915113788@qq.com>
2024-03-22 14:09:45 +0800

    feature： 修改Mybatis 的自动填充的字段名为驼峰形式

--- project/src/main/java/com/nian/shortlink/project/config/MyMetaObjectHandler.java
+++ project/src/main/java/com/nian/shortlink/project/config/MyMetaObjectHandler.java
@@ -7,18 +7,27 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+/**
+ * Mybatis 的自动填充基本数据
+ */
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         //数据库中的创建时间与更新时间都是date类型的，所以后面需要设置成Date的字节码文件
-        strictInsertFill(metaObject, "create_time", Date::new, Date.class);
-        strictInsertFill(metaObject, "update_time", Date::new, Date.class);
-        strictInsertFill(metaObject, "del_flag", () -> 0, Integer.class);
+//        metaObject.setValue("createTime", new Date());
+//        metaObject.setValue("updateTime", new Date());
+//        metaObject.setValue("delFlag", new Date());
+        strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        strictInsertFill(metaObject, "update_time", Date::new, Date.class);
+        //直接通过set方法赋值updateTime当前时间
+//        metaObject.setValue("updateTime", new Date());
+//        strictUpdateFill(metaObject, "updateTime", Date::new, Date.class);
+        strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
     }
 }
