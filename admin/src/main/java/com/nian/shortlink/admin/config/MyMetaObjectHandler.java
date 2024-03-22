d0bc26694d52c19d05b2e4464ef67a84b8fd8bf1
nianshangy <915113788@qq.com>
2024-03-22 14:10:46 +0800

    feature： 修改Mybatis 的自动填充的字段名为驼峰形式

--- admin/src/main/java/com/nian/shortlink/admin/config/MyMetaObjectHandler.java
+++ admin/src/main/java/com/nian/shortlink/admin/config/MyMetaObjectHandler.java
@@ -12,13 +12,14 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         //数据库中的创建时间与更新时间都是date类型的，所以后面需要设置成Date的字节码文件
-        strictInsertFill(metaObject, "create_time", Date::new, Date.class);
-        strictInsertFill(metaObject, "update_time", Date::new, Date.class);
-        strictInsertFill(metaObject, "del_flag", () -> 0, Integer.class);
+        strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        strictInsertFill(metaObject, "update_time", Date::new, Date.class);
+        //直接通过set方法赋值updateTime当前时间
+        strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
     }
 }
